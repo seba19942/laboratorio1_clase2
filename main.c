4312012eeca1b0e599871f3bce379f63eea59c06
seba19942 <47338028+seba19942@users.noreply.github.com>
2019-04-10 20:38:33 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CANTIDAD_REP 3
+#include <string.h>
 
-int PedirAcumularMostrar();
+int getString(  char *resultado,
+                char *msg,
+                char *msgError,
+                int minimo,
+                int maximo,
+                int reintentos);
+
+///
+int getNumber(  int *resultado,
+                char *msg,
+                char *msgError,
+                int minimo,
+                int maximo,
+                int reintentos);
+
+int isValidNumber (char* cadena);
+
+
+
+
+)
+
+
+int main()
 {
 
-    int acumulador=0;
-    int buffer;
-    int i;
-    for(i=0;i<CANTIDAD_REP;i++)
+    printf("Hello world!\n");
+    return 0;
+}
 
-    printf("ingrese numero %d: ",i+1);
-    scanf("%d",&buffer);
-    //falta validar
-    acumulador = acumulador + buffer;
+int getString(  char *pResultado,
+                char *msg,
+                char *msgError,
+                int minimo,
+                int maximo,
+                int reintentos)
 
-    printf("El promedio es: %f",(float)acumulador / CANTIDAD_REP);
+{
+    char bufferStr[4096];
+
+    fgets(bufferStr,sizeof(bufferStr),stdin);
+    bufferStr[strlen(bufferStr)-1] = '\0';
+
 
     return 0;
 
+}
 
-
-
+int isValidIntNumber (char* cadena)
+{
+    return 1;
 
 }
 
+int getNumber(  int *resultado,
+                char *msg,
+                char *msgError,
+                int minimo,
+                int maximo,
+                int reintentos)
+{
+    int retorno = -1;
+    char bufferStr[4096];
+    int bufferInt;
 
-
-
-
-
+    if( resultado != NULL &&
+        reintentos >= 0) //FALTAN
+    {
+        if(!getString(bufferStr,msg,msgError,1,6,1) &&
+            isValidIntNumber(bufferStr))
+        {
+            bufferInt = atoi(bufferStr);
+            if(bufferInt >= minimo && bufferInt <= maximo)
+            {
+                retorno = 0;
+                *resultado = bufferInt;
+            }
+        }
+    }
+    return retorno;
+}
 
 
 
